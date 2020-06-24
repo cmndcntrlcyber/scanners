467ef7a5cb72c9aeda7493a5045aa4552e87ae13
cmndcntrlcyber <47669879+cmndcntrlcyber@users.noreply.github.com>
2020-06-23 21:52:09 -0500

    Create git-all.sh

---
+++ git-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+mkdir /home/d43dl3u5/git
+cd /home/d43dl3u5/git
+
+git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
+git clone https://github.com/1N3/IntruderPayloads.git
+git clone https://github.com/fuzzdb-project/fuzzdb.git
+git clone https://github.com/The-Art-of-Hacking/h4cker.git
+git clone https://github.com/OlivierLaflamme/Cheatsheet-God.git
