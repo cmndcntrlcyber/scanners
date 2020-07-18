0897149f49340fd5e5e09d025b13d05802e6fc58
cmndcntrlcyber <47669879+cmndcntrlcyber@users.noreply.github.com>
2020-07-18 12:20:57 -0400

    Update git-all.sh

--- git-all.sh
+++ git-all.sh
@@ -1,10 +1,18 @@
-#!/bin/bash/
-
-mkdir /home/d43dl3u5/git
-cd /home/d43dl3u5/git
+mkdir /home/d43d3lu5/git
+cd /home/d43d3lu5/git
 
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 git clone https://github.com/1N3/IntruderPayloads.git
 git clone https://github.com/fuzzdb-project/fuzzdb.git
 git clone https://github.com/The-Art-of-Hacking/h4cker.git
 git clone https://github.com/OlivierLaflamme/Cheatsheet-God.git
+git clone https://github.com/danielmiessler/SecLists.git
+
+mv IntruderPayloads INTpay
+mv PayloadsAllTheThings PATT
+mv SecLists seclists
+
+mv fuzzdb /usr/share/wordlists
+mv INTpay /usr/share/wordlists
+mv PATT /usr/share/wordlists
+mv seclists /usr/share/wordlists
