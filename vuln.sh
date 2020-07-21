fc69d4f10ff4e4702515d3cc1b1510951e050fda
cmndcntrlcyber <47669879+cmndcntrlcyber@users.noreply.github.com>
2020-07-21 18:30:06 -0500

    Update vuln.sh

--- vuln.sh
+++ vuln.sh
@@ -11,22 +11,32 @@ sudo mkdir /home/d43d3lu5/rslts/$name
 sudo mkdir /home/d43d3lu5/rslts/$name/vuln
 cd /home/d43d3lu5/rslts/$name/vuln
 
-sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O -oX vuln-init.$name.xml
-xsltproc vuln-init.$name.xml -o vuln-init.$name.html
+sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script smb-brute.nse -oX vuln.smb-brute.$name.xml
+xsltproc vuln.smb-brute.$name.xml -o vuln.smb-brute.$name.html
+rm vuln.smb-brute.$name.xml
 
-sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script vulners --script-args mincvss=5.0  -oX vuln.vulners.$name.xml
+sudo nmap $target $ports -T5 -v5 -Pn -sV -sC -oX vuln.init.$name.xml
+xsltproc vuln.init.$name.xml -o vuln.init.$name.html
+rm vuln.init.$name.xml 
+
+sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script vulners --script-args mincvss=5.0 -oX vuln.vulners.$name.xml
 xsltproc vuln.vulners.$name.xml -o vuln.vulners.$name.html
+rm vuln.vulners.$name.xml 
 
-sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script dns-srv-enum  --script-args "dns-srv-enum.domain"  -oX vuln.dns-enum.$name.xml
-xsltproc vuln.dns-enum.$name.xml -o vuln.dns-enum.$name.html
-
-sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script exploit --script-args "exploit.intensive" -oX vuln.exploit.$name.xml
-xsltproc vuln.exploit.$name.xml -o vuln.exploit.$name.html
-
-sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script http-enum --script-args "http-enum.category" -oX vuln.http-enum.$name.xml
-xsltproc vuln.http-enum.$name.xml -o vuln.http-enum.$name.html
-
-sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script ssh-publickey-acceptance -oX vuln.ssh-pubkey.$name.xml
-xsltproc vuln.ssh-pubkey.$name.xml -o vuln.ssh-pubkey.$name.html
-
+sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script dns-srv-enum  --script-args "dns-srv-enum.domain" -oX vuln.dns-enum.$name.xml
+xsltproc vuln.dns-enum.$name.xml -o vuln.dns-enum.$name.html                                                                                                                                                                               
+rm vuln.dns-enum.$name.xml                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                           
+sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script exploit --script-args "exploit.intensive" -oX vuln.exploit.$name.xml                                                                                                           
+xsltproc vuln.exploit.$name.xml -o vuln.exploit.$name.html                                                                                                                                                                                 
+rm vuln.exploit.$name.xml                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                           
+sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script http-enum --script-args "http-enum.category" -oX vuln.http-enum.$name.xml                                                                                                      
+xsltproc vuln.http-enum.$name.xml -o vuln.http-enum.$name.html                                                                                                                                                                             
+rm vuln.http-enum.$name.xml                                                                                                                                                                                                                
+                                                                                                                                                                                                                                           
+sudo nmap $target $ports -f -T5 -v5 -Pn -sV -sC -O --script ssh-publickey-acceptance -oX vuln.ssh-pubkey.$name.xml                                                                                                                         
+xsltproc vuln.ssh-pubkey.$name.xml -o vuln.ssh-pubkey.$name.html                                                                                                                                                                           
+rm vuln.ssh-pubkey.$name.xml                                                                                                                                                                                                               
+                                                                                                                                                                                                                                           
 echo "Done!"
