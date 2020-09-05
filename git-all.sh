df7a2bd8e374f18e28b6eeb66711f1b20c0101d3
cmndcntrlcyber <cmndcntrlcyber@gmail.com>
2020-09-05 03:31:53 -0500

    Update git-all.sh

--- git-all.sh
+++ git-all.sh
@@ -1,7 +1,11 @@
 #!/bin/bash/
 
-mkdir /home/d43d3lu5/git
-cd /home/d43d3lu5/git
+echo "what is your username"
+read user
+
+mkdir /home/$user/scripts
+mkdir /home/$user/git
+cd /home/$user/git
 
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 git clone https://github.com/1N3/IntruderPayloads.git
@@ -9,11 +13,20 @@ git clone https://github.com/fuzzdb-project/fuzzdb.git
 git clone https://github.com/The-Art-of-Hacking/h4cker.git
 git clone https://github.com/OlivierLaflamme/Cheatsheet-God.git
 git clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/cmndcntrlcyber/scanners.git
+git clone https://github.com/cmndcntrlcyber/payloads.git
+git clone https://github.com/cmndcntrlcyber/py.git
+git clone https://github.com/cmndcntrlcyber/commands
+
 
 mv IntruderPayloads INTpay
 mv PayloadsAllTheThings PATT
 mv SecLists seclists
 
+mv scanners /home/d43d3lu5/scripts
+mv paylaods /home/d43d3lu5/scripts
+mv py /home/d43d3lu5/scripts
+mv commands /home/d43d3lu5
 mv fuzzdb /usr/share/wordlists
 mv INTpay /usr/share/wordlists
 mv PATT /usr/share/wordlists
